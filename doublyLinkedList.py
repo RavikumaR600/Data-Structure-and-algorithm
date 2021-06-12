980fc9342f094dff20d0f685a2577356947098e6
unknown <67065363+RavikumaR600@users.noreply.github.com>
2021-06-12 21:45:11 +0530

    Update doublyLinkedList.py

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -97,13 +97,68 @@ class douly_LinkedList:
                     self.head= new_node
                 n.pref = new_node
 
+    def delete_begin(self):
+        if self.head is None:
+            print("douly_LinkedList is empty")
+            return 
+        if self.head.nref is None:
+            self.head = None
+            print("douly_LinkedList is empty so can't delete")
+        else:
+            self.head = self.head.n.ref 
+            self.pref = None
+
+    def delete_end(self):
+        if self.head is None:
+            print("douly_LinkedList is empty")
+            return 
+        if self.head.nref is None:
+            self.head = None
+            print("douly_LinkedList is empty so can't delete")
+        else:
+            n= self.head
+            while n.nref is not None:
+                n=n.nref
+            n.pref.nref = None
+
+    def delete_by_value(self,x):
+        if self.head is None:
+            print("douly_LinkedList is empty")
+            return 
+        if self.head.nref is None:
+            if x==self.head.data:
+                self.head = None
+        else:
+            print("x is not present is DLL")
+            return
+        if self.head.data == x:
+            self.head =self.head.nref
+            self.head.pref = None
+            return
+        n=self.head
+        while n.nref is not None:
+            if x==n.data:
+                break
+            n=n.nref
+        if n.nref is not None:
+            n.nref.pref =n.pref 
+            n.pref.nref= n.nref
+        else:
+            if n.data ==x:
+                n.pref.nref = None 
+            else:
+                print("x is not in DLL")
+
 
 dll= douly_LinkedList()
-dll.add_before(600,200)
-dll.add_begin(100)
-dll.add_begin(200)
-dll.add_after(400,200)
-dll.insert_empty(10)
-dll.insert_empty(20)
+#  dll.add_before(600,200)
+# dll.add_begin(100)
+# dll.add_begin(200)
+#  dll.add_after(400,200)
+#  dll.insert_empty(10)
+#  dll.insert_empty(20)
 # dll.print_ll_rev()
-dll.print_ll()
+# dll.delete_begin()
+# dll.delete_end()
+# dll.delete_by_value(200)
+# dll.print_ll()
