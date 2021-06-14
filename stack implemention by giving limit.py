0981d68bc155884e34dd4ee200b85449ab4680aa
unknown <67065363+RavikumaR600@users.noreply.github.com>
2021-06-14 23:28:32 +0530

    Create stack implemention by giving limit.py

---
+++ stack implemention by giving limit.py
@@ -0,0 +1,30 @@
+stack=[]
+def push():
+    if len(stack)==n:
+        print("list is full!")
+    else:
+        element=input("enter the element: ")
+        stack.append(element)
+        print(stack)
+def pop_element():
+    if not stack:
+        print("Stack is empty! ")
+    else:
+        e=stack.pop()
+        print("removed elment :",e)
+        print(stack)
+n=int(input("enter the limit of the stack"))
+while True:
+    print('''select the operations:
+    1.push
+    2.pop
+    3.Quit ''')
+    choice= int(input())
+    if choice==1:
+        push()
+    elif choice==2:
+        pop_element()
+    elif choice==3:
+        break
+    else:
+        print("enter the correct")
